3a0c4341022846659c15ebfaac0c2f6b42f46d8f
Christopher Henderson <chris.hend012@gmail.com>
2024-04-11 15:40:32 -0300

    feat: add drawer menu

--- app/(home)/_components/MenuBar/index.tsx
+++ app/(home)/_components/MenuBar/index.tsx
@@ -2,8 +2,11 @@ import Image from 'next/image'
 import ButtonAnimation from '../ButtonAnimation'
 import { SiWhatsapp } from 'react-icons/si'
 import Link from 'next/link'
+import { useMediaQuery } from '@/hooks/use-media-query'
+import MobileMenu from '../MobileMenu'
 
 const MenuBar = () => {
+	const isDesktop = useMediaQuery('(min-width: 768px)')
 	const url =
 		'https://api.whatsapp.com/send?phone=551640423781&text=Ol%C3%A1%20Livty%20Ingressos,%20eu%20vim%20do%20seu%20site%20e%20gostaria%20de%20falar%20com%20um%20representante!'
 	return (
@@ -17,25 +20,29 @@ const MenuBar = () => {
 				</Link>
 			</div>
 
-			<div className="row-span-1 md:col-span-6 flex items-center">
-				<ul className="w-full flex justify-center flex-wrap gap-2 md:gap-6">
-					<li className="animateMenu">
-						<Link href="/"> Início </Link>
-					</li>
-					<li className="animateMenu">
-						<a href="#gestao"> Gestão Facilitada </a>
-					</li>
-					<li className="animateMenu">
-						<Link href="#livty">Livty Pay</Link>
-					</li>
-					<li className="animateMenu">
-						<Link href="#feedback">Feedbacks</Link>
-					</li>
-					<li className="animateMenu">
-						<Link href="#eventos">Eventos</Link>
-					</li>
-				</ul>
-			</div>
+			{!isDesktop ? (
+				<MobileMenu />
+			) : (
+				<div className="row-span-1 md:col-span-6 flex items-center">
+					<ul className="w-full flex justify-center flex-wrap gap-2 md:gap-6">
+						<li className="animateMenu">
+							<Link href="/"> Início </Link>
+						</li>
+						<li className="animateMenu">
+							<a href="#gestao"> Gestão Facilitada </a>
+						</li>
+						<li className="animateMenu">
+							<Link href="#livty">Livty Pay</Link>
+						</li>
+						<li className="animateMenu">
+							<Link href="#feedback">Feedbacks</Link>
+						</li>
+						<li className="animateMenu">
+							<Link href="#eventos">Eventos</Link>
+						</li>
+					</ul>
+				</div>
+			)}
 
 			<div className="row-span-1 md:col-span-3 flex items-center justify-center">
 				<Link href={url}>
--- app/(home)/_components/MobileMenu/index.tsx
+++ app/(home)/_components/MobileMenu/index.tsx
@@ -1,7 +1,58 @@
 'use client'
 
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerTrigger,
+} from '@/components/ui/drawer'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState } from 'react'
+
 const MobileMenu = () => {
-	return null
+	const [isMenuOpen, setisMenuOpen] = useState(false)
+	const links = [
+		{ path: '/', label: 'Início' },
+		{ path: '#gestao', label: 'Gestão Facilitada' },
+		{ path: '#livty', label: 'Livty Pay' },
+		{ path: '#feedback', label: 'Feedbacks' },
+		{ path: '#eventos', label: 'Eventos' },
+	]
+
+	const handleMenu = () => {
+		setisMenuOpen(!isMenuOpen)
+	}
+	return (
+		<Drawer
+			direction="left"
+			open={isMenuOpen}>
+			<DrawerTrigger onClick={handleMenu}>icone</DrawerTrigger>
+			<DrawerContent className="border-none">
+				<Link href="/">
+					<Image
+						src={require('../../../../public/image/Logo.webp')}
+						alt="Logo"
+					/>
+				</Link>
+
+				<ul className="space-y-4 mt-8">
+					{links.map((link) => (
+						<li
+							className="animateMenu"
+							key={link.label}
+							onClick={handleMenu}>
+							<Link href={link.path}>{link.label}</Link>
+						</li>
+					))}
+				</ul>
+
+				<DrawerClose>
+					<Link href={links[2].path}>Testeee</Link>
+				</DrawerClose>
+			</DrawerContent>
+		</Drawer>
+	)
 }
 
 export default MobileMenu
